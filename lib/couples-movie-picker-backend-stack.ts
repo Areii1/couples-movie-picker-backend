41691159be7cd32491ffa79c96e3d98b41235720
ari-pekka jääskeläinen <ari.jaaskelainen1@gmail.com>
2021-01-13 10:23:11 +0200

    fix profile picture uploading process issues

--- lib/couples-movie-picker-backend-stack.ts
+++ lib/couples-movie-picker-backend-stack.ts
@@ -4,6 +4,8 @@ import * as lambda from "@aws-cdk/aws-lambda";
 import * as apigw from "@aws-cdk/aws-apigateway";
 import cognito = require("@aws-cdk/aws-cognito");
 import s3 = require("@aws-cdk/aws-s3");
+import { CfnIdentityPoolRoleAttachment } from "@aws-cdk/aws-cognito";
+import { Construct } from "@aws-cdk/core";
 
 export class CouplesMoviePickerBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -54,10 +56,25 @@ export class CouplesMoviePickerBackendStack extends cdk.Stack {
       generateSecret: false,
     });
 
-    new cognito.CfnIdentityPool(this, "couples-movie-picker-identity-pool", {
-      allowUnauthenticatedIdentities: false,
-      identityPoolName: "couples-movie-picker-identity-pool",
-    });
+    // need to connect identitypool to user pool and app client
+    // need to add roles for unauth and auth
+
+    const identityPool = new cognito.CfnIdentityPool(
+      this,
+      "couples-movie-picker-identity-pool",
+      {
+        allowUnauthenticatedIdentities: true,
+        // cognitoIdentityProviders: [
+        //   { clientId: "3kc1hghrf2lkvrfehrjog7st1d" },
+        //   { providerName: "eu-central-1_f7dMA4Naz" },
+        // ],
+      }
+    );
+
+    // const identityPoolUnauthenticatedRole = new CfnIdentityPoolRoleAttachment(this, 'couples-movie-picker-identity-pool-unauthenticated-role', {
+    //   identityPoolId: identityPool.id,
+    //   roles:
+    // });
 
     const movies = new dynamodb.Table(this, "movies", {
       partitionKey: { name: "title", type: dynamodb.AttributeType.STRING },
@@ -81,14 +98,32 @@ export class CouplesMoviePickerBackendStack extends cdk.Stack {
       },
     });
 
-    const profilePictureUploadedTrigger = new lambda.Function(this, "ProfilePictureUploadedTrigger", {
-      runtime: lambda.Runtime.NODEJS_10_X,
-      code: new lambda.AssetCode("src"),
-      handler: "ProfilePictureUploadedTrigger.handler",
-      environment: {
-        USERS_TABLE_NAME: usersTable.tableName,
-      },
-    });
+    const profilePictureUploadedTrigger = new lambda.Function(
+      this,
+      "ProfilePictureUploadedTrigger",
+      {
+        runtime: lambda.Runtime.NODEJS_10_X,
+        code: new lambda.AssetCode("src"),
+        handler: "profilePictureUploadedTrigger.handler",
+        environment: {
+          USERS_TABLE_NAME: usersTable.tableName,
+        },
+      }
+    );
+
+    const removeProfilePicture = new lambda.Function(
+      this,
+      "RemoveProfilePictureHandler",
+      {
+        runtime: lambda.Runtime.NODEJS_10_X,
+        code: new lambda.AssetCode("src"),
+        handler: "removePicture.handler",
+        environment: {
+          USERS_TABLE_NAME: usersTable.tableName,
+        },
+      }
+    );
+    usersTable.grantReadWriteData(profilePictureUploadedTrigger as any);
 
     const api = new apigw.RestApi(this, "couples-movie-picker-api", {
       restApiName: "couples-movie-picker-api",
@@ -101,6 +136,15 @@ export class CouplesMoviePickerBackendStack extends cdk.Stack {
     userResource.addMethod("GET", getUsersIntegration);
     usersTable.grantReadWriteData(getUsers as any);
 
+    const pictureResource = userResource.addResource("picture");
+    addCorsOptions(pictureResource);
+
+    const removeProfilePictureIntegration = new apigw.LambdaIntegration(
+      removeProfilePicture
+    );
+    pictureResource.addMethod("GET", removeProfilePictureIntegration);
+    usersTable.grantReadWriteData(removeProfilePicture as any);
+
     const addUserIntegration = new apigw.LambdaIntegration(addUser);
     userResource.addMethod("POST", addUserIntegration);
     usersTable.grantReadWriteData(addUser as any);
@@ -112,8 +156,41 @@ export class CouplesMoviePickerBackendStack extends cdk.Stack {
     moviesResource.addMethod("POST", likeMovieIntegration);
     usersTable.grantReadWriteData(likeMovie as any);
 
-
-    const profilePicturesBucket = new s3.Bucket(this, "profilePicturesBucket");
+    // need to add bucket policy to s3 bucket
+    //   {
+    //     "Version": "2012-10-17",
+    //     "Statement": [
+    //         {
+    //             "Sid": "PublicReadGetObject",
+    //             "Effect": "Allow",
+    //             "Principal": "*",
+    //             "Action": [
+    //                 "s3:GetObject",
+    //                 "s3:ListBucket",
+    //                 "s3:PutObject"
+    //             ],
+    //             "Resource": [
+    //                 "arn:aws:s3:::couplesmoviepickerbacken-profilepicturesbucketa8b-wzbj5zhprz9k",
+    //                 "arn:aws:s3:::couplesmoviepickerbacken-profilepicturesbucketa8b-wzbj5zhprz9k/*"
+    //             ]
+    //         }
+    //     ]
+    // }
+    const profilePicturesBucket = new s3.Bucket(this, "profilePicturesBucket", {
+      cors: [
+        {
+          allowedOrigins: ["*"],
+          allowedMethods: [
+            s3.HttpMethods.GET,
+            s3.HttpMethods.DELETE,
+            s3.HttpMethods.POST,
+            s3.HttpMethods.PUT,
+            s3.HttpMethods.HEAD,
+          ],
+          allowedHeaders: ["*"],
+        },
+      ],
+    });
   }
 }
 
